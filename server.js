0c4b2082547762484f0e0088c40d02aee8dab5ab
Ann-Marie <anniedillier@gmail.com>
2020-11-17 21:59:42 -0800

    start up

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
-const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({ helpers });
+// express app
+const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
+
+// use express-session cookie for user authentication
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
-  secret: "Super secret secret",
+  secret: "encryption on (dummy text)",
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -23,13 +31,8 @@ const sess = {
 app.use(session(sess));
 
 app.use(express.json());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
-
-// turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-});
